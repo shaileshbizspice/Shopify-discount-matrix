0ef3983ad8107166d5269e6a3a8a0a98e2b26848
Shailesh <shailesh@bizspice.com>
2026-02-04 18:11:17 +0530

    Changes in the migration file

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -16,5 +16,5 @@ CREATE TABLE "Session" (
     "collaborator" BOOLEAN DEFAULT false,
     "emailVerified" BOOLEAN DEFAULT false,
     "refreshToken" TEXT,
-    "refreshTokenExpires" TIMESTAMP,
+    "refreshTokenExpires" TIMESTAMP
 );
