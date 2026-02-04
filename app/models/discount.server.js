595dae41beb29bd0882150b0839d653bfb13bbd4
Shailesh <shailesh@bizspice.com>
2026-02-04 15:36:08 +0530

    Ensure the discount create once

--- app/models/discount.server.js
+++ app/models/discount.server.js
@@ -70,6 +70,7 @@ export async function ensureDiscount(request) {
         shippingDiscounts: true,
       },
       startsAt: new Date().toISOString(),
+      "discountClasses": ["PRODUCT"]
     },
   };
 
